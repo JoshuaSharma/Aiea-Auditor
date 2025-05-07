04d68580f707aae1241be12d82817a3e866b6eab
Joshua Sharma <josharma@ucsc.edu>
2025-05-06 19:09:02 -0700

    bruh

--- Task-5/prologue.pl
+++ Task-5/prologue.pl
@@ -1,20 +1,15 @@
 :- dynamic conducts_electricity/1.
+:- dynamic metal/1.
+:- dynamic insulator/1.
 :- dynamic made_of/2.
-:- dynamic material_type/2.
 
 % Facts
-material_type(metal, conducts_electricity).
-material_type(insulator, does_not_conduct_electricity).
-material_type(iron, metal).
+metal(metals).
+insulator(insulators).
+conducts_electricity(metals).
+conducts_electricity(X) :- metal(X).
 made_of(nails, iron).
+metal(iron).
 
-% Rules
-conducts_electricity(X) :-
-    made_of(X, Material),
-    material_type(Material, Type),
-    Type == conducts_electricity.
-
-does_not_conduct_electricity(X) :-
-    made_of(X, Material),
-    material_type(Material, Type),
-    Type == does_not_conduct_electricity.
+% Query: Are nails conductive?
+conducts_electricity(nails) :- made_of(nails, X), metal(X).
