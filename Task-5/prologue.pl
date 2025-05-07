df720aea2ecce7466f75806f25513e12ed40bba2
Joshua Sharma <josharma@ucsc.edu>
2025-05-06 19:03:16 -0700

    something happpened

--- Task-5/prologue.pl
+++ Task-5/prologue.pl
@@ -1,13 +1,15 @@
 % Facts
-conducts(metal, electricity).
-not(conducts(insulator, electricity)).
+conducts_electricity(metal).
+does_not_conduct_electricity(insulator).
+made_of(iron, metal).
+made_of(nail, iron).
 
 % Rules
-metal(X) :- made_of(X, iron).
-conducts(X, electricity) :- metal(X).
-
-% Specific facts
-made_of(nails, iron).
+conducts_electricity(Object) :- made_of(Object, Material), conducts_electricity(Material).
 
 % Query
-query(nails_conducts_electricity) :- conducts(nails, electricity).
+% To check if nails cannot conduct electricity, we negate the conducts_electricity predicate.
+% Answer the query: does_not_conduct_electricity(nail) or conduct_electricity(nail).
+
+% The statement "Nails cannot conduct electricity." is false because nails are made of iron,
+% and iron is a metal, and metals conduct electricity.
