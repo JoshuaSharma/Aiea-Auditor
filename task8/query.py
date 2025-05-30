905686ab2551dfd8108fb29a38c670a8bab1a2dc
Ziyu-Zhou <zhouziyv15@gmail.com>
2025-05-29 18:56:02 -0700

    langchain

--- task8/query.py
+++ task8/query.py
@@ -4,13 +4,51 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from pyswip import Prolog
 from functions import extract_code  # your custom function
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
 
 
+# manuel RAG
 def retrieve_relevant_kb(question, kb_file="animals_kb.pl"):
+
+    # Break the question into lowercase terms
     terms = question.lower().split()
-    with open(kb_file) as f:
-        lines = f.readlines()
-    return "\n".join([line for line in lines if any(term in line.lower() for term in terms)])
+
+    # Read the knowledge base file
+    with open(kb_file, "r") as file:
+        kb_lines = file.readlines()
+
+    # Collect lines that contain any of the terms
+    relevant_lines = []
+    for line in kb_lines:
+        line_lower = line.lower()
+        if any(term in line_lower for term in terms):
+            relevant_lines.append(line)
+
+    # Return the matching lines as a single string
+    return "\n".join(relevant_lines)
+
+# langchain RAG
+def retrieve_relevant_context_with_rag(question, kb_file="animals_kb.pl"):
+    # Load and split KB
+    loader = TextLoader(kb_file)
+    docs = loader.load()
+    splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+    split_docs = splitter.split_documents(docs)
+
+    # Embed and store in FAISS
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(split_docs, embeddings)
+    retriever = vectorstore.as_retriever()
+
+    # Get relevant context
+    relevant_docs = retriever.get_relevant_documents(question)
+    return "\n".join(doc.page_content for doc in relevant_docs)
+
+
 
 
 def main():
@@ -45,9 +83,19 @@ def main():
         facts_prompt = PromptTemplate.from_template(
             "You are a helpful assistant that outputs raw Prolog code from this question: {question}"
         )
-        facts = llm.invoke(facts_prompt.format(question=question)).content
 
+        # this doesn't use the facts in database but asking for Chatgpt 
+        # an good example is ask something that's not true in general but true 
+        # according to knowledge base to test out
+        # facts = llm.invoke(facts_prompt.format(question=question)).content
+
+        # this will make sure chatgpt use the knowledge base 
+        # manuel RAG
         # facts = retrieve_relevant_kb(question)
+
+        # langchain RAG
+        facts = retrieve_relevant_context_with_rag(question)
+
 
         
         # with open("prologue.pl", "w") as f:
